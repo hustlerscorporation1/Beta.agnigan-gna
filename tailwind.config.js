827356e8aab54f9827fcb899ced9be6290f955b0
010julien <adoboejulien6@gmail.com>
2025-08-23 13:12:39 +0000

    Resolution de bug de deploiement 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // analyse tous les fichiers JS/TS/JSX/TSX dans src
   ],
   safelist: [
-    "w-1/2",
-    "w-1/3",
-    "w-2/3",
-    "h-1/2",
-    "h-1/3",
-    "h-2/3",
-    "text-1/2",
-    "text-1/3",
-    "text-2/3",
+    "w-1\\/2",
+    "w-1\\/3",
+    "w-2\\/3",
+    "h-1\\/2",
+    "h-1\\/3",
+    "h-2\\/3",
+    "text-1\\/2",
+    "text-1\\/3",
+    "text-2\\/3",
   ],
   theme: {
     extend: {
-      colors: {
+      colors: {s
         primary: "#146c54",
         secondary: "#0b0b2b",
         accent: "#00bcd4",
