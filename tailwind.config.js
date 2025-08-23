5b9bff71891247d23b8d38c275e2288a86a8599d
010julien <adoboejulien6@gmail.com>
2025-08-23 15:18:07 +0000

    Resoudre Bug 23082025

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // tous tes composants
+  ],
   theme: {
     extend: {},
   },
